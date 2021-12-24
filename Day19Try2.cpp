a05de95fddfebd153b04d4a88294f6f9d019a10e
Albin Mamuti <amamuti@student.ethz.ch>
2021-12-24 11:13:18 +0100

    D24 by hand

--- Day19Try2.cpp
+++ Day19Try2.cpp
@@ -9,128 +9,175 @@
 
 using namespace std;
 class Beacon;
-class Beacon{
-    public:
-    int x,y,z;
+class Beacon
+{
+public:
+    int x, y, z;
     Beacon(){};
-    Beacon(int _x, int _y, int _z) {x = _x; y = _y; z = _z;}
-    string offsetHash(Beacon b) { stringstream s; s << "[" << b.x-x << "," << b.y-y << "," << b.z-z << "]"; return s.str(); }
-    int offset(Beacon b) { return abs(b.x - x) + abs(b.y -y ) + abs(b.z -z);}
-    bool operator==(const Beacon & b) { return (x == b.x ) && (y == b.y) && (z == b.z);}
-    bool operator< (const Beacon & b) { return (x < b.x); }
+    Beacon(int _x, int _y, int _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+    string offsetHash(Beacon b)
+    {
+        stringstream s;
+        s << "[" << b.x - x << "," << b.y - y << "," << b.z - z << "]";
+        return s.str();
+    }
+    int offset(Beacon b) { return std::abs(b.x - x) + std::abs(b.y - y) + std::abs(b.z - z); }
+    bool operator==(const Beacon &b) { return (x == b.x) && (y == b.y) && (z == b.z); }
+    bool operator<(const Beacon &b) { return (x < b.x); }
 };
-#define IT(i, x, y, z) case i: for (auto b: ref) s.push_back(Beacon(x, y, z)); break;
-vector<Beacon> getIteration(vector<Beacon> ref, int iteration) {
+#define IT(i, x, y, z)                    \
+    case i:                               \
+        for (auto b : ref)                \
+            s.push_back(Beacon(x, y, z)); \
+        break;
+vector<Beacon> getIteration(vector<Beacon> ref, int iteration)
+{
     vector<Beacon> s;
-    switch (iteration) {
-        IT( 0, b.x, b.y, b.z) IT( 1, b.x,-b.z, b.y) IT( 2, b.x,-b.y,-b.z) IT( 3, b.x, b.z,-b.y)
-        IT( 4, b.y, b.z, b.x) IT( 5, b.y,-b.x, b.z) IT( 6, b.y,-b.z,-b.x) IT( 7, b.y, b.x,-b.z)
-        IT( 8, b.z, b.x, b.y) IT( 9, b.z,-b.y, b.x) IT(10, b.z,-b.x,-b.y) IT(11, b.z, b.y,-b.x)
-        IT(12,-b.z,-b.y,-b.x) IT(13,-b.z, b.x,-b.y) IT(14,-b.z, b.y, b.x) IT(15,-b.z,-b.x, b.y)
-        IT(16,-b.y,-b.x,-b.z) IT(17,-b.y, b.z,-b.x) IT(18,-b.y, b.x, b.z) IT(19,-b.y,-b.z, b.x)
-        IT(20,-b.x,-b.z,-b.y) IT(21,-b.x, b.y,-b.z) IT(22,-b.x, b.z, b.y) IT(23,-b.x,-b.y, b.z)
+    switch (iteration)
+    {
+
+        IT(0, b.x, b.y, b.z)
+        IT(1, b.x, -b.z, b.y)
+        IT(2, b.x, -b.y, -b.z)
+        IT(3, b.x, b.z, -b.y)
+            IT(4, b.y, b.z, b.x) IT(5, b.y, -b.x, b.z) IT(6, b.y, -b.z, -b.x) IT(7, b.y, b.x, -b.z)
+                IT(8, b.z, b.x, b.y) IT(9, b.z, -b.y, b.x) IT(10, b.z, -b.x, -b.y) IT(11, b.z, b.y, -b.x)
+                    IT(12, -b.z, -b.y, -b.x) IT(13, -b.z, b.x, -b.y) IT(14, -b.z, b.y, b.x) IT(15, -b.z, -b.x, b.y)
+                        IT(16, -b.y, -b.x, -b.z) IT(17, -b.y, b.z, -b.x) IT(18, -b.y, b.x, b.z) IT(19, -b.y, -b.z, b.x)
+                            IT(20, -b.x, -b.z, -b.y) IT(21, -b.x, b.y, -b.z) IT(22, -b.x, b.z, b.y) IT(23, -b.x, -b.y, b.z)
     }
     return s;
 };
 
-Beacon findTrans(vector<Beacon> a, vector <Beacon> b, string offset){
-    for(auto i: a){
-        for( auto j : b){
-            if(i.offsetHash(j) == offset)
-                return Beacon( i.x-j.x, i.y-j.y, i.z-j.z );
+Beacon findTrans(vector<Beacon> a, vector<Beacon> b, string offset)
+{
+    for (auto i : a)
+    {
+        for (auto j : b)
+        {
+            if (i.offsetHash(j) == offset)
+                return Beacon(i.x - j.x, i.y - j.y, i.z - j.z);
         }
     }
     return Beacon();
 }
 
-int main(int argc, char * argv[]){
+int main(int argc, char *argv[])
+{
 
-    ifstream input; string line;
-    if (argc < 2) { 
+    ifstream input;
+    string line;
+    if (argc < 2)
+    {
         input.open("inputPartOne.txt");
-    }else
+    }
+    else
         input.open(argv[1]);
-    
+
     queue<vector<Beacon>> scanners;
     vector<Beacon> scanner, grid;
-    while (!input.eof()) {
+    while (!input.eof())
+    {
         std::getline(input, line);
-        if (line.find("scanner") != string::npos) {
-            if (scanner.size() > 0) scanners.push(scanner);
+        if (line.find("scanner") != string::npos)
+        {
+            if (scanner.size() > 0)
+                scanners.push(scanner);
             scanner = vector<Beacon>();
-        } else if (line == "") {
+        }
+        else if (line == "")
+        {
             continue;
-        } else {
+        }
+        else
+        {
             vector<int> vals;
             int c1 = line.find(",");
-            int c2 = line.find(",", c1+1);
-            int x = stoi(line.substr(0,c1));
-            int y = stoi(line.substr(c1+1, c2-c1));
-            int z = stoi(line.substr(c2+1));
+            int c2 = line.find(",", c1 + 1);
+            int x = stoi(line.substr(0, c1));
+            int y = stoi(line.substr(c1 + 1, c2 - c1));
+            int z = stoi(line.substr(c2 + 1));
             scanner.push_back(Beacon(x, y, z));
         }
     }
-    if (scanner.size() > 0) scanners.push(scanner);
+    if (scanner.size() > 0)
+        scanners.push(scanner);
     grid = scanners.front();
     std::sort(grid.begin(), grid.end());
     scanners.pop(); // initialize scanner 0 as root for finding all others position
 
     vector<Beacon> scannerPos;
-    scannerPos.push_back(Beacon(0,0,0)); // first scanner is at pos 0,0,0
+    scannerPos.push_back(Beacon(0, 0, 0)); // first scanner is at pos 0,0,0
 
-    while(!scanners.empty()){
+    while (!scanners.empty())
+    {
         vector<Beacon> current = scanners.front();
         scanners.pop();
 
-        pair<int, int> maxPair = {-1,0};
+        pair<int, int> maxPair = {-1, 0};
         string maxOff = "";
 
-        for(int i = 0; i < 24; i++) // 24 Perm
+        for (int i = 0; i < 24; i++) // 24 Perm
         {
             map<string, int> offSet;
-            map<int,int> count;
-            vector<Beacon> iteration = getIteration(current,i);
-            for(auto ref : grid){
-                for(auto b : iteration){
+            map<int, int> count;
+            vector<Beacon> iteration = getIteration(current, i);
+            for (auto ref : grid)
+            {
+                for (auto b : iteration)
+                {
                     offSet[ref.offsetHash(b)]++;
                 }
             }
-            for(auto dist : offSet){
+            for (auto dist : offSet)
+            {
                 count[dist.second]++;
             }
-            for(auto c : count){
-                if(c.first >= 12 && c.first > maxPair.second)//Probably the second part is not tested
+            for (auto c : count)
+            {
+                if (c.first >= 12 && c.first > maxPair.second) // Probably the second part is not tested
                 {
-                    maxPair = {i,c.first};
-                    for(auto n : offSet)
-                        if(n.second == c.first)
+                    maxPair = {i, c.first};
+                    for (auto n : offSet)
+                        if (n.second == c.first)
                             maxOff = n.first;
                 }
             }
         }
-        if(maxPair.first == -1){
+        if (maxPair.first == -1)
+        {
             scanners.push(current); // nothing found in this round
             continue;
         }
 
-        vector<Beacon> matching = getIteration(current,maxPair.first);
-        Beacon t = findTrans(grid,matching,maxOff); 
+        vector<Beacon> matching = getIteration(current, maxPair.first);
+        Beacon t = findTrans(grid, matching, maxOff);
         int dupecount = 0;
         vector<Beacon> candidates;
         // we want to find the transformation so that it matches other points in grid
-        for(auto m : matching){
-            Beacon candidate = Beacon(m.x+t.x,m.y+t.y,m.z+t.z);
+        for (auto m : matching)
+        {
+            Beacon candidate = Beacon(m.x + t.x, m.y + t.y, m.z + t.z);
             bool dupe = false;
-            for(auto g : grid){
-                if(candidate == g){
+            for (auto g : grid)
+            {
+                if (candidate == g)
+                {
                     dupe = true;
                     dupecount++;
                     break;
                 }
             }
-            if(!dupe) candidates.push_back(candidate); //these are beacons not already in grid
+            if (!dupe)
+                candidates.push_back(candidate); // these are beacons not already in grid
         }
-        if(dupecount < maxPair.second){
+        if (dupecount < maxPair.second)
+        {
             scanners.push(current);
             continue;
         }
@@ -139,12 +186,13 @@ int main(int argc, char * argv[]){
         newGrid.erase(unique(newGrid.begin(), newGrid.end()), newGrid.end());
         grid = newGrid;
         scannerPos.push_back(t);
+        cout << "Progress" << endl;
     }
     cout << grid.size() << endl;
     int maxDist = -1;
-    for(auto l: scannerPos)
-    for(auto r: scannerPos)
-        if(l.offset(r) > maxDist)
-            maxDist = l.offset(r);
+    for (auto l : scannerPos)
+        for (auto r : scannerPos)
+            if (l.offset(r) > maxDist)
+                maxDist = l.offset(r);
     cout << maxDist << endl;
 }
---
+++ Day24P1P2.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+#include <set>
+#include <chrono>
+using namespace std::chrono;
+using namespace std;
+typedef unsigned long long lld;
+
+char MODO[15] = {'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '\0'};
+int main()
+{
+    ifstream input("C:\\Users\\Albin\\OneDrive\\Desktop\\AoC\\AoC\\inputPartOne.txt");
+    lld w, x, y, z;
+    string line;
+    int pos = 0;
+    vector<int> div;
+    vector<int> check;
+    vector<int> OFFSET;
+    for (int k = 0; k < 14; k++)
+    {
+        for (int i = 0; i < 18; i++)
+        {
+            getline(input, line);
+            if (i == 0)
+                continue;
+            if (i == 4)
+            {
+                int temp;
+                sscanf(line.c_str(), "div z %d", &temp);
+                div.push_back(temp);
+            }
+            else if (i == 5)
+            {
+                int temp;
+                sscanf(line.c_str(), "add x %d", &temp);
+                check.push_back(temp);
+            }
+            else if (i == 15)
+            {
+                int temp;
+                sscanf(line.c_str(), "add y %d", &temp);
+                OFFSET.push_back(temp);
+            }
+        }
+    }
+    printf("DIV | CHE | OFF\n");
+    for (int i = 0; i < 14; i++)
+    {
+        printf("%3d | %3d | %3d \n", div[i], check[i], OFFSET[i]);
+    }
+    MODO[4] = '9'; // p1
+    MODO[3] = '8';
+
+    MODO[6] = '2';
+    MODO[5] = '9';
+
+    MODO[8] = '9';
+    MODO[9] = '4';
+
+    MODO[7] = '9';
+    MODO[10] = '6';
+
+    MODO[2] = '9';
+    MODO[11] = '1';
+
+    MODO[1] = '5';
+    MODO[12] = '9';
+
+    MODO[0] = '4';
+    MODO[13] = '9';
+    printf("Sol P1: %s\n", MODO);
+
+    MODO[4] = '2'; // p2
+    MODO[3] = '1';
+
+    MODO[6] = '1';
+    MODO[5] = '8';
+
+    MODO[8] = '6';
+    MODO[9] = '1';
+
+    MODO[7] = '4';
+    MODO[10] = '1';
+
+    MODO[2] = '9';
+    MODO[11] = '1';
+
+    MODO[1] = '1';
+    MODO[12] = '5';
+
+    MODO[0] = '1';
+    MODO[13] = '6';
+    printf("Sol P2: %s\n", MODO);
+}
